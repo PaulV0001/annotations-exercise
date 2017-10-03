137f95d99197d299b44b682cd25900d99eb6e05f
paulvaldez <valdezpaul00@gmail.com>
2017-10-03 09:16:31 -0500

    Completed fixing the warnings with the @SupressWarnings and complied and ran it again.

--- src/Author.java
+++ src/Author.java
@@ -15,15 +15,15 @@ public class Author extends Person {
     /**
      * @deprecated Use publishedBooks instead
      */
-    @Deprecated
+    @SuppressWarnings({"deprecated", "unchecked"})
     public List<String> getBooks() {
         return books;
     }
-
+    @SuppressWarnings("unchecked")
     public List<String> publishedBooks() {
         return books;
     }
-
+    @SuppressWarnings("unchecked")
     public void addBook(String book) {
         books.add(book);
     }
